772904e0596b2096a7ed2187c17e3cd39ca603fd
gorbatschow <gorbatschow.r@gmail.com>
2023-03-09 23:41:20 +0300

    add stop watch timer class

---
+++ ImAppStopWatch.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <algorithm>
+#include <chrono>
+
+namespace ImApp {
+enum class StopWatchType { Normal, Max };
+template <StopWatchType TimerType> class StopWatchTimer {
+public:
+  StopWatchTimer() {}
+
+  inline void start() {
+    _beginTime = std::chrono::high_resolution_clock::now();
+  }
+
+  inline void stop() {
+    _endTime = std::chrono::high_resolution_clock::now();
+    if constexpr (_timerType == StopWatchType::Normal) {
+      _us = std::chrono::duration_cast<std::chrono::microseconds>(_endTime -
+                                                                  _beginTime)
+                .count();
+    } else if constexpr (_timerType == StopWatchType::Max) {
+      _us = std::max(_us, std::chrono::duration_cast<std::chrono::microseconds>(
+                              _endTime - _beginTime)
+                              .count());
+    }
+  }
+
+  inline long microseconds() const { return _us; }
+
+private:
+  std::chrono::system_clock::time_point _beginTime{}, _endTime{};
+  long _us{};
+  static constexpr StopWatchType _timerType{TimerType};
+};
+} // namespace ImApp
